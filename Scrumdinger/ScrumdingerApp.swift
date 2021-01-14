6a68afb2459d5ad573f4a8eca5438122cbc7ac0e
Jan Bjelicic <jan.bjelicic@sunhill-technologies.com>
2021-01-14 10:57:39 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jan Bjelicic on 14/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
