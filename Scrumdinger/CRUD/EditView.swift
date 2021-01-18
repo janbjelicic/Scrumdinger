a27108adb0c73091515e80a64c39dbdda27d2af9
Jan Bjelicic <jan.bjelicic@sunhill-technologies.com>
2021-01-18 16:57:11 +0100

    Initial few steps of the course, creating CRUD, meeting, model

---
+++ Scrumdinger/CRUD/EditView.swift
@@ -0,0 +1,20 @@
+//
+//  EditView.swift
+//  Scrumdinger
+//
+//  Created by Jan Bjelicic on 18/01/2021.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditView()
+    }
+}
